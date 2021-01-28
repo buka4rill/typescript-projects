c7c419e8be6d012624c0c4a7966954ac734ec91e
Ebuka Abraham <buka4rill@gmail.com>
2021-01-28 13:53:57 +0100

    Defining FC properly in TS, adding diff prop type

--- rts/src/props/Child.tsx
+++ rts/src/props/Child.tsx
@@ -1,7 +1,26 @@
 interface ChildProps {
     color: string;
+    onClick: () => void; // A function whose return type is void
 }
 
-export const Child = ({ color }: ChildProps) => {
-    return <div> {color} </div>
+export const Child = ({ color, onClick }: ChildProps) => {
+    return <div> 
+        {color}
+        <button onClick={onClick}> Click me</button> 
+    </div>
 }
+
+// The problem with this is that Typescript doesn't understand 
+// that we are defining a React Component.
+
+
+// Second way of defining Function components in Typescript
+export const ChildAsFC: React.FC<ChildProps> = ({ color, onClick }) => {
+    return <div> 
+        {color} 
+        <button onClick={onClick}> Click me</button> 
+    </div>
+    
+};
+
+// Properties now available etc ChildAsFC.propTypes, ChildAsFC.displayName
